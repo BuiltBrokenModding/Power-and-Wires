4ac255a220832098125d63ab584736f4176c986e
Robert Seifert <rseifert.phone@gmail.com>
2017-05-30 02:38:15 -0400

    init files

---
+++ src/main/java/com/builtbroken/paw/PowerAndWiresMod.java
@@ -0,0 +1,10 @@
+package com.builtbroken.paw;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 5/30/2017.
+ */
+public class PowerAndWiresMod
+{
+    public static final String DOMAIN = "powerandwires";
+}
